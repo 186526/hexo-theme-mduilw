ebae882e8b01cae30a67976dbe2f5ff568f4f148
186526 <186526@186526.club>
2020-05-20 22:12:03 +0800

    fix Valine bug with pjax enable

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,9 @@
 var mainbody = document.getElementById("main-body");
 var darkmod = 0;
-var imgs=document.getElementsByTagName('img');
+add_pic = function () {var imgs=document.getElementsByTagName('img');
 for (var i=0; i<imgs.length; i++){
     imgs[i].classList.add('mdui-img-fluid')
-}
+};};
 if (window.matchMedia('(prefers-color-scheme: dark)').matches){
     mainbody.classList.add("mdui-theme-layout-dark");
     darkmod = 1
